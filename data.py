e76bae8979fb704d449833d167a9ec3af4c21740
joshua-miles <joshua@christetulit.org>
2018-10-30 17:17:49 -0500

    something

---
+++ data.py
@@ -0,0 +1,44 @@
+pies = [
+
+    {
+        'name': 'Pumpkin',
+        'price': 10.99,
+        'weight': '1lb 12oz',
+        'color': 'rgb(240, 156, 29)'
+    },
+
+    {
+        'name': 'Apple',
+        'price': 14.99,
+        'weight': '2lb 14oz',
+        'color': 'rgb(246, 208, 151)'
+    },
+
+    {
+        'name': 'Peach',
+        'price': 14.99,
+        'weight': '3lb 0oz',
+        'color': 'rgb(253, 219, 118)'
+    },
+
+    {
+        'name': 'Cherry',
+        'price': 14.99,
+        'weight': '2lb 14oz',
+        'color': 'rgb(195, 8, 11)'
+    },
+
+    {
+        'name': 'Blueberry',
+        'price': 13.99,
+        'weight': '3lb 6oz',
+        'color': 'rgb(29, 19, 112)'
+    },
+
+    {
+        'name': 'Chocolate Pecan',
+        'price': 19.99,
+        'weight': '2lb 5oz',
+        'color': 'rgb(101, 73, 35)'
+    }
+]
--- server.py
+++ server.py
@@ -10,4 +10,14 @@ if __name__ == '__main__':
 
 @socketio.on('connect')
 def handle_connection():
-   emit('data', app.output)
+   emit('data', {
+    "analysis": {
+        "total":app.total,
+        "most_cost_effective":app.most_cost_effective
+    },
+    "charts": {
+        "pies_by_cost": app.pies_by_cost,
+        "pies_by_weight": app.pies_by_weight,
+        "pies_by_cost_per_pound": app.pies_by_cost_per_pound
+    }
+})
